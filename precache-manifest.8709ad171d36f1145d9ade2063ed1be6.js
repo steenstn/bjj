070d0cc082abf742680295403fa032cbbf66ba55
Alexander Steen <alexander.steen@academicwork.se>
2018-10-18 12:28:07 +0200

    Updates

---
+++ precache-manifest.8709ad171d36f1145d9ade2063ed1be6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bjj/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "aaa3d1dbc96b90e43166",
+    "url": "/bjj/static/js/runtime~main.aaa3d1db.js"
+  },
+  {
+    "revision": "af580a730e6558bd9a25",
+    "url": "/bjj/static/js/main.af580a73.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/bjj/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "af580a730e6558bd9a25",
+    "url": "/bjj/static/css/main.9f38c23a.chunk.css"
+  },
+  {
+    "revision": "09a35ff0bccbdc99c0ab1ce5c1accbe7",
+    "url": "/bjj/index.html"
+  }
+];
