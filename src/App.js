b64cec0f2fe96a6040e4e91fb11b6474ad189b92
Alexander Steen <alexander.steen@academicwork.se>
2018-10-18 12:21:13 +0200

    Call backend

--- src/App.js
+++ src/App.js
@@ -3,25 +3,53 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      sessions: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://bjjtraining.herokuapp.com/trainingsessions")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            sessions: result
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    const { error, isLoaded, sessions } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {sessions.map(session => (
+            <li>
+              {session.date} {session.trainingType} {session.lengthMin}
+            </li>
+          ))}
+        </ul>
+      );
+    }
   }
 }
 
