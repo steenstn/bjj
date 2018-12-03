ccd29eac6815d081b4e69b3ea342cc1b75d63b75
Nathalie Agnekil <nathalie@agnekil.com>
2018-12-03 11:34:12 +0100

    #9 changed from a-tag to Link in LoginForm

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class LoginForm extends Component {
     constructor(props) {
@@ -58,7 +59,7 @@ class LoginForm extends Component {
                 <input id="password" name="password" type="password" />
                 <button>Login</button>
                 </form>
-                <a href="/register"><button>Register new user</button></a>
+                <Link to="/register"><button>Register new user</button></Link>
             </div>
         );
     }
